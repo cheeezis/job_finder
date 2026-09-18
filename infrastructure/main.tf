5334d28524fe53225856e409ac5a9b500164b6ec
Jannis <cheeezis@users.noreply.github.com>
2026-09-18 08:50:19 +0200

    infra: add Container Apps environment and log workspace

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,3 +17,29 @@ resource "azurerm_container_registry" "jobfinder" {
 
   tags = azurerm_resource_group.jobfinder.tags
 }
+
+resource "azurerm_log_analytics_workspace" "jobfinder" {
+  name                = "law-jobfinder"
+  resource_group_name = azurerm_resource_group.jobfinder.name
+  location            = azurerm_resource_group.jobfinder.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+
+  tags = azurerm_resource_group.jobfinder.tags
+}
+
+resource "azurerm_container_app_environment" "jobfinder" {
+  name                = "cae-jobfinder"
+  resource_group_name = azurerm_resource_group.jobfinder.name
+  location            = azurerm_resource_group.jobfinder.location
+
+  logs_destination           = "log-analytics"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.jobfinder.id
+
+  workload_profile {
+    name                  = "Consumption"
+    workload_profile_type = "Consumption"
+  }
+
+  tags = azurerm_resource_group.jobfinder.tags
+}
